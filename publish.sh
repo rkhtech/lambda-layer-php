7b9daf191b4365713d49f37e2f9cfdb612ddc153
Randy Hommel <randy@hommel.name>
2022-08-17 09:32:17 -0700

    changing script due to bucket settings changes

--- publish.sh
+++ publish.sh
@@ -12,9 +12,9 @@ version=$(cat Dockerfile | grep '^ENV PHP_VERSION' | awk '{print $3}')
 echo "PHP_VERSION: $version"
 
 if [ -f "php-$version.zip" ]; then
-  aws s3 cp php-$version.zip s3://rkh-pub/lambda-layers/php-${version}.zip --acl public-read --profile rkhtech
+  aws s3 cp php-$version.zip s3://rkh-pub/lambda-layers/php-${version}.zip --profile rkhtech
   if [ "$1" == "php81" ]; then
-    aws s3 cp s3://rkh-pub/lambda-layers/php-${version}.zip s3://rkh-pub/lambda-layer-php/php-latest.zip --acl public-read --profile rkhtech
+    aws s3 cp s3://rkh-pub/lambda-layers/php-${version}.zip s3://rkh-pub/lambda-layer-php/php-latest.zip --profile rkhtech
   fi
 else
   echo "file doesnt exist, can't publish, try building it first."

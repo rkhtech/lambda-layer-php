e41ef38f06c7d1457d9bb758db1f041dc410cfac
Randy Hommel <randy@hommel.name>
2022-10-27 09:48:40 -0700

    Bump to PHP 8.1.11

--- publish.sh
+++ publish.sh
@@ -13,9 +13,11 @@ echo "PHP_VERSION: $version"
 
 if [ -f "php-$version.zip" ]; then
   aws s3 cp php-$version.zip s3://rkh-pub/lambda-layers/php-${version}.zip --profile rkhtech
-  if [ "$1" == "php81" ]; then
-    aws s3 cp s3://rkh-pub/lambda-layers/php-${version}.zip s3://rkh-pub/lambda-layer-php/php-latest.zip --profile rkhtech
-  fi
+#  if [ "$1" == "php81" ]; then
+    aws s3 cp s3://rkh-pub/lambda-layers/php-${version}.zip s3://rkh-pub/lambda-layers/php-latest.zip --profile rkhtech
+  aws s3 cp phpinfo.txt s3://rkh-pub/lambda-layers/phpinfo-${version}.txt
+  aws s3 cp phpinfo.txt s3://rkh-pub/lambda-layers/phpinfo-latest.txt
+#  fi
 else
   echo "file doesnt exist, can't publish, try building it first."
 fi
